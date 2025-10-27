531762291d6cc1380086e91c2ebeb1e927823f80
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-10-27 07:52:08 +0100

    chore: add sa

--- snippet/src/app/snippets/new/page.tsx
+++ snippet/src/app/snippets/new/page.tsx
@@ -1,6 +1,27 @@
+import db from "@/src/db";
+import { redirect } from "next/navigation";
+
 export default function CreateSnippetPage() {
+  async function createFormData(formData: FormData) {
+    "use server";
+
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    console.log(snippet);
+
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={createFormData}>
       <h3 className="font-bold m-3">Create a snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
