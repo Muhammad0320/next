37998aec0898fb2f984f745d1d96630fa4063404
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-10-27 09:16:47 +0100

    chore: update page

--- snippet/app/snippets/[id]/page.tsx
+++ snippet/app/snippets/[id]/page.tsx
@@ -1,3 +1,20 @@
-export function ShowSnippetPage() {
-  return <div>A Snippet Page</div>;
+import db from "@/db";
+import { notFound } from "next/navigation";
+
+interface ShowSnippetPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export async function ShowSnippetPage(props: ShowSnippetPageProps) {
+  const snippet = await db.snippet.findFirst({
+    where: { id: Number.parseInt(props.params.id) },
+  });
+
+  if (!snippet) {
+    return notFound;
+  }
+
+  return <div> {snippet.title} </div>;
 }
