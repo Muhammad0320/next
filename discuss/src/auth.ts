44a2cbcae81e615fb14f5f0e566d1223956a8af4
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-10-31 08:56:50 +0100

    chore: add auth ts file

---
+++ discuss/src/auth.ts
@@ -0,0 +1,6 @@
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+  throw new Error("Missing github oauth credentials");
+}
