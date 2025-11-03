b960e57a1ba269774f87f98fa8d693172732e0d3
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-11-03 09:45:47 +0100

    chore: diplay for validation

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -1,5 +1,38 @@
 "use server";
 
-export const createTopic = async () => {
+import { z } from "zod";
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, "Name must be lowercase letters or dashes without space"),
+  description: z.string().min(10),
+});
+
+export const createTopic = async (
+  errors: CreateTopicFormState,
+  formdata: FormData
+): Promise<CreateTopicFormState> => {
+  const result = createTopicSchema.safeParse({
+    name: formdata.get("name"),
+    description: formdata.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  return { errors: {} };
+
   // TODO: revalidate the home page
 };
