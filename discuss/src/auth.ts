0f332ee138f5ed2cd3af6265773d8f6eb49549c7
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-11-04 07:26:36 +0100

    {your_commit_message}

--- discuss/src/auth.ts
+++ discuss/src/auth.ts
@@ -2,16 +2,24 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import db from "./db";
 import Github from "next-auth/providers/github";
-
+import type { Session, User } from "next-auth";
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
-  console.log("--------------------- Help shit just happend? ");
+  console.log("--------------------- Help! shit just happend? ");
   throw new Error("Missing github oauth credentials");
 }
 
 console.log(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET);
+
+declare module "next-auth" {
+  interface Session {
+    user: User & {
+      id: string;
+    };
+  }
+}
 
 export const {
   handlers: { GET, POST },
