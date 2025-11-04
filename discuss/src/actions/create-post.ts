da1331b4be13b7dedd200e9c4f9e3c13e570fa79
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-11-04 08:13:36 +0100

    chore: update the creae post sa

--- discuss/src/actions/create-post.ts
+++ discuss/src/actions/create-post.ts
@@ -1,5 +1,64 @@
 "use server";
 
-export const createPost = async () => {
+import db from "@/db";
+import z from "zod";
+
+interface CreatePostFormStatus {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+export const createPost = async (
+  slug: string,
+  formStatus: CreatePostFormStatus,
+  formData: FormData
+): Promise<CreatePostFormStatus> => {
+  /////////////// NOTE: commented because the nextAuth doensn't return anything
+  //   const session = await auth()
+
+  // if(!session !! !session.user) {
+  //   return  {
+  //     errors: {
+  //       _form: ["You are nott allowed to do this."]
+  //     }
+  //   }
+  // }
+
+  const topic = await db.topic.findUnique({
+    where: { slug },
+    select: { id: true },
+  });
+
+  if (!topic) {
+    return {
+      errors: {
+        _form: ["Cannot find topic"],
+      },
+    };
+  }
+
+  const results = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+
+  if (!results.success) {
+    return {
+      errors: results.error.flatten().fieldErrors,
+    };
+  }
+
+  return {
+    errors: {},
+  };
+
   // Todo: Revalidate the topic show page
 };
