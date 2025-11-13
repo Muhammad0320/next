7775775e15635c455a9c738da996b264a1a8b872
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-11-13 18:07:45 +0100

    chore: update code

--- chat/src/app/components/MobileMenu.tsx
+++ chat/src/app/components/MobileMenu.tsx
@@ -1,15 +1,16 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
 
 export default function MobileMenu() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="md:hidden">
+    <div className="md:hidden relative">
       <button
         onClick={() => setIsOpen((prev) => !prev)}
-        className="flex flex-col justify-center items-center"
+        className="flex flex-col justify-center items-center z-20 relative"
       >
         <span
           className={`block transition-all duration-300 ease-out h-1 w-6 rounded-sm bg-blue-500 ${
@@ -27,6 +28,16 @@ export default function MobileMenu() {
           }`}
         ></span>
       </button>
+      {isOpen && (
+        <div className="absolute top-16 left-0 w-full h-[calc(100vh-4rem)] bg-white text-black flex flex-col items-center justify-center gap-8 text-xl z-10">
+          <Link href="/">Home</Link>
+          <Link href="/">Friends</Link>
+          <Link href="/">Groups</Link>
+          <Link href="/">stories</Link>
+          <Link href="/">profile</Link>
+          <Link href="/">Links</Link>
+        </div>
+      )}
     </div>
   );
 }
